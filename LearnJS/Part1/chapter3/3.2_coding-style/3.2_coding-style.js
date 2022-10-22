f6dae1505bb6be56d85f54846ea3933a1ecf1f23
Даниил Уткин <dan4ezzz@gmail.com>
2022-10-22 15:33:51 +0300

    chapter 3 completed

--- LearnJS/Part1/chapter3/3.2_coding-style/3.2_coding-style.js
+++ LearnJS/Part1/chapter3/3.2_coding-style/3.2_coding-style.js
@@ -1,5 +1,39 @@
 // https://learn.javascript.ru/coding-style
 
+// Линтер - инструмент для проверки стиля кода (актуальный на 2022 - ESLint)
+
+/* 
+ * Базовые стайлгайды
+ * 
+ * Google JavaScript Style Guide - https://google.github.io/styleguide/javascriptguide.xml
+ * Airbnb JavaScript Style Guide - https://github.com/airbnb/javascript     ( перевод - https://leonidlebedev.github.io/javascript-airbnb/ )
+ * Idiomatic.JS - https://github.com/rwaldron/idiomatic.js                  ( перевод - https://github.com/rwaldron/idiomatic.js/tree/master/translations/ru_RU )
+ * StandardJS - https://standardjs.com/
+*/
+
+
+
+// Пример документации функций
+/**
+ * Represents a book.
+ * @constructor
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ */
+ function Book(title, author) {
+}
+/**
+ * Returns x, raised to the nth power.
+ *
+ * @param {number} x Возводимое в степень число.
+ * @param {number} n A number that can be raised to a power.
+ * @return {number} x, raised to the nth power.
+ */
+ function pow(x, n) {
+}
+
+
+
 // Task 1
 function pow(x,n)
 {
---
+++ LearnJS/Part1/chapter3/3.5_testing-mocha/testing-mocha.js
@@ -0,0 +1,122 @@
+// https://learn.javascript.ru/testing-mocha
+
+/* 
+ * Фреймворки для тестирования js (браузер и сервер)
+ * 
+ * Mocha – основной фреймворк. Общие функции тестирования, такие как `describe()`, `it()` и `assert`       ( https://mochajs.org/ ).
+ * Chai – библиотека, с множеством методов assert.                                                         ( https://www.chaijs.com/ )
+ * Sinon – библиотека, позволяющая наблюдать за функциями, эмулировать встроенные функции и многое другое  ( https://sinonjs.org/ ).
+ * Karma – высокоуровневый фреймворк с возможностью автозапуска тестов                                     ( https://karma-runner.github.io )
+*/
+
+/* 
+ * Методы функции `assert`
+ *  
+ * Проверка, что переданное значение равно NaN -  `.isNaN()` 
+ * Проверяет value1 == value2 -                   `.equal(value1, value2)`
+ * Проверяет value1 === value2 -                  `.strictEqual(value1, value2)`
+ * Проверяет value1 != value2 -                   `.notEqual(value1, value2)`
+ * Проверяет value1 != value2 -                   `.notStrictEqual(value1, value2)`
+ * Проверяет value === true -                     `.isTrue(value)`
+ * Проверяет value === false -                    `.isFalse(value)`
+*/
+
+
+// Хорошей практикой считается сначала писать тесты, потом писать функционал
+// Спецификация - то, что код должен делать и тесты:
+  describe("pow", function() {                          // Заголовок - группирует тесты
+
+    it("возводит в степень n", function() {             // Имя теста (что тестируем?)
+      assert.equal(pow(2, 3), 8);                       // Тест 
+      assert.equal(pow(3, 3), 27);                      // Тест (если предыдущий fail - скипает этот и запускает след. `it()`)
+    });
+    it("возводит в степень n", function() {             // Если добавить метод `.only` сразу после `it`, выполнится только этот тест, `.skip` пропустит тест
+      assert.equal(pow(2, 3), 8);                       
+    });
+
+  }); // Также можно обернуть `it()` в функцию и запустить в цикле для автоматизации вставки значений
+      // Но! если так делать, то для наглядности рез-та и инкапсуляции зацикленной функции, лучше все это обернуть в еще один `describe()` (вложенный, без доп `it()`)
+
+
+// В теле `describe()` существуют функции, которые будут выполняться до/после тестов, 
+// используются для инициализации, обнуления счётчиков или чего-нибудь ещё между тестами:
+  before(() => alert("1 – перед всеми тестами"));
+  after(() => alert("4 – после всех тестов"));
+  beforeEach(() => alert("2 – перед тестом"));
+  afterEach(() => alert("3 – после теста"));          // Расположение в функции не влияет на порядок их выполнения
+
+
+
+
+// **Импорт Mocha и Chai в браузер (в header шаблона)**:
+  // добавим стили mocha для отображения результатов 
+  '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mocha/3.2.0/mocha.css">'
+
+  // добавляем сам фреймворк mocha 
+  '<script src="https://cdnjs.cloudflare.com/ajax/libs/mocha/3.2.0/mocha.js"></script>'
+
+  '<script>'
+    // включаем режим тестирования в стиле BDD
+    'mocha.setup("bdd");'
+  '</script>'
+
+  // добавим chai 
+  '<script src="https://cdnjs.cloudflare.com/ajax/libs/chai/3.5.0/chai.js"></script>'
+
+  '<script>'
+    //  Объявим assert chai глобально
+    'let assert = chai.assert;'
+  '</script>'
+
+
+// **Реализация тестов в браузере (в body шаблона)**:
+  '<script>'
+  'function pow(x, n) {'
+    // Здесь будет реализация функции
+  '}'
+  '</script>'
+
+  // скрипт со спецификацией (describe, it...)
+  '<script src="test.js"></script>'
+
+  // элемент с id="mocha" будет содержать результаты тестов
+  '<div id="mocha"></div>'
+
+  // запускаем тесты!
+  '<script>'
+  'mocha.run();'
+  '</script>'
+
+
+
+// Task 1
+// Что не так?
+it("Возводит x в степень n", function() {
+  let x = 5;
+
+  let result = x;
+  assert.equal(pow(x, 1), result);
+
+  result *= x;
+  assert.equal(pow(x, 2), result);
+
+  result *= x;
+  assert.equal(pow(x, 3), result);
+});
+
+// Решение
+describe('Pow', () => {
+
+  it('5 to the power of 1 will be 5', () => {
+    assert.equal(pow(5, 1), 5);
+  });
+
+  it('5 to the power of 2 will be 25', () => {
+    assert.equal(pow(5, 2), 25);
+  });
+
+  it('5 to the power of 3 will be 125', () => {
+    assert.equal(pow(5, 3), 125);
+  });
+
+});
