d611a8ec1359905259aebba9a1d2c5ba980ea537
Даниил Уткин <dan4ezzz@gmail.com>
2022-11-29 15:18:19 +0300

    add Hint typesCheck

---
+++ Hints/typesCheck.js
@@ -0,0 +1,17 @@
+// Проверки для всех типов
+
+// Number
+// BigInt
+// String
+// null
+// undefined
+// Symbol
+// Bool
+// Object
+// Array
+// Map
+// Set
+// WeakMap
+// WeakSet
+
+// (в процессе...)
