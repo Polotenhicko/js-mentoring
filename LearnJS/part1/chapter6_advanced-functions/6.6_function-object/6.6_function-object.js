35368936d3503a90fb92adaebb4635c567ca6cf6
Даниил Уткин <dan4ezzz@gmail.com>
2022-12-05 15:22:30 +0300

    add 6.6 without practice

---
+++ LearnJS/part1/chapter6_advanced-functions/6.6_function-object/6.6_function-object.js
@@ -0,0 +1,110 @@
+// https://learn.javascript.ru/function-object
+
+// Функции можно не только вызывать, но и добавлять/изменять их св-ва, получать к ним доступ, как к объектам.
+
+
+
+// Св-во функции 'name' содержит имя функции. Имя присваивается из контекста.
+const foo = function () {return 'gaga'};
+console.log(foo.name);            // Output: 'foo'
+const fooArrow = () => 'arrow gaga';
+console.log(fooArrow.name);       // Output: 'fooArrow'
+
+// Логично, что имена также имеют методы объекта
+const obj = {
+  methodExample() {
+    return 'it\'s a method';
+  },
+};
+console.log(obj.methodExample.name);       // Output: 'methodExample'
+
+
+
+// Св-во 'length' содержит кол-во параметров функции при объявлении (rest не считается)
+const foo2 = function (a, b, c, d, ...rest) {
+  return a + b + c + d + rest;
+};
+console.log(foo2.length);       // Output: 4
+console.log(foo.length);        // Output: 0
+
+
+
+// Можно добавлять свои св-ва (св-ва НЕ есть переменная)
+function foo3 () {
+  console.log('foo3 called');
+  if (foo3.counter) {
+    foo3.counter++;
+  } else {
+    foo3.counter = 1;
+  }
+}
+foo3();
+foo3();
+foo3();
+console.log(`total foo3 calls: ${foo3.counter}`);      // Output: 'total foo3 calls: 3'
+
+
+
+// `Named Function Expression (NFE)` - синтаксис для динамического 'ссылания' объекта на себя (this в мире функций). Не работает с Function Declaration.
+const foo4 = function func(name) {
+  return name ? `name is: ${name}` : func('guest');
+}
+const foo5 = foo4;
+console.log(foo5());      // Output: 'name is: guest'
+
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Task 1
+// Реализуйте makeCounter(), создающий функцию со счетчиком вызова так, чтобы счётчик мог уменьшать и устанавливать значение:
+// counter() должен возвращать следующее значение (как и раньше).
+// counter.set(value) должен устанавливать счётчику значение value.
+// counter.decrease() должен уменьшать значение счётчика на 1.
+function makeCounter () {
+  return function counter() {
+    console.log('counter called');
+
+    if (counter.value) {
+      counter.value++
+    } else {
+      counter.value = 1;
+    }
+
+    counter.set = function (value) {
+      counter.value = value;
+    };
+
+    counter.decrease = function () {
+      counter.value--;
+    };
+  }
+}
+const counter = makeCounter();
+counter();
+counter();
+counter();
+counter();
+console.log(`total counter calls: ${counter.value}`);      // Output: 'total counter calls: 4'
+
+
+
+// Task 2
+// Напишите функцию sum, которая бы работала следующим образом:
+console.log(+sum(1)(2));                              // Output: 3
+console.log(+sum(1)(2)(3));                        // Output: 6
+console.log(+sum(5)(-1)(2));                       // Output: 6
+console.log(+sum(6)(-1)(-2)(-3));               // Output: 0
+console.log(+sum(0)(1)(2)(3)(4)(5));      // Output: 15
+
+function sum (n) {
+
+  const addMore = function (x) {
+    return sum(n + x);
+  };
+
+  addMore.valueOf = function() {
+    return n;
+  };
+
+  return addMore;
+}
