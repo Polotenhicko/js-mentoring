045d4bdfbc056b96938af8d7755547405e829804
Даниил Уткин <dan4ezzz@gmail.com>
2022-12-08 01:42:28 +0300

    6.8 completed + microwave task

--- LearnJS/part1/chapter6_advanced-functions/6.8_settimeout-setinterval/6.8_settimeout-setinterval.js
+++ LearnJS/part1/chapter6_advanced-functions/6.8_settimeout-setinterval/6.8_settimeout-setinterval.js
@@ -1,7 +1,7 @@
 // https://learn.javascript.ru/settimeout-setinterval
 
 // `setTimeout(fn, delay = 0, ...args?)`        - Вызовет fn(...args)` через delay мсек
-setTimeout((x) => console.log(x), 1000, 'resolved' );     // Output: 'resolved' (after 1 sec)
+setTimeout((x) => console.log(x), 1000, 'resolved');     // Output: 'resolved' (after 1 sec)
 
 
 // Если вложить setTimeout в setTimeout, то вложенный запланируется сразу после выполнения предыдущего
@@ -12,7 +12,7 @@ setTimeout(function () {
 
 
 // вызов `setTimeout()` возвращает id таймаута (в node.js - объект)
-const timeout = setTimeout((x) => console.log(x), 2000, 'resolved 101010' );
+const timeout = setTimeout((x) => console.log(x), 2000, 'resolved 101010');
 console.log(timeout);          // Output: -timeout object-
 console.log(+timeout);         // Output: 6 (asyncId)
 
@@ -40,17 +40,53 @@ function recursionTimeout() {
     recursionTimeout();
   }, 5000);
 }
+
 recursionTimeout();
 
 
 
 // ---------------------------------------------------------------------------------------------------------------------
 // `setInterval(fn, interval = 0, ...args?)`    - Будет вызывать fn(...args) через каждые delay мсек
-const interval = setInterval((x) => console.log(x), 1000, 'cycled' );     // Output: 'cycled' (every 1 sec)
+const interval = setInterval((x) => console.log(x), 1000, 'cycled');     // Output: 'cycled' (every 1 sec)
 
 // `clearInterval(interval)` - остановить циклические вызовы
-setTimeout( () => clearInterval(interval), 5000);
+setTimeout(() => clearInterval(interval), 5000);
 
 
 
 // ---------------------------------------------------------------------------------------------------------------------
+// Task 1
+// Напишите функцию printNumbers(from, to), которая выводит число каждую секунду, начиная от from и заканчивая to.
+// Сделайте два варианта решения.
+//    - Используя setInterval.
+//    - Используя рекурсивный setTimeout.
+const printNumbers1 = function (from, to) {
+  let i = 1;
+  setInterval(() => console.log(i++), 1000);
+};
+printNumbers1();
+
+const printNumbers2 = function (i = 1) {
+
+  setTimeout(function () {
+    console.log(i);
+    printNumbers2(i + 1);
+  }, 1000);
+
+};
+printNumbers2();
+
+
+
+// Task 2
+// В приведённом ниже коде запланирован вызов setTimeout, а затем выполняется сложное вычисление, для завершения которого требуется более 100 мс.
+// Когда будет выполнена запланированная функция?   - После цикла, тк асинхрон выполняется после синхронного кода
+
+// Что покажет console.log?
+let i = 0;
+setTimeout(() => console.log('task 2 '+ i), 100);      // Output: 100_000_000
+
+// предположим, что время выполнения этой функции >100 мс
+for(let j = 0; j < 100_000_000; j++) {
+  i++;
+}
