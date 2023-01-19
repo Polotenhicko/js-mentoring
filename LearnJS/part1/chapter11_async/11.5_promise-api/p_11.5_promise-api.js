b2abd72e6b0070f1003d06b94f00f9000a9c876e
Даниил Уткин <dan4ezzz@gmail.com>
2023-01-19 20:20:58 +0300

    add 9.1 practice

--- LearnJS/part1/chapter11_async/11.5_promise-api/p_11.5_promise-api.js
+++ LearnJS/part1/chapter11_async/11.5_promise-api/p_11.5_promise-api.js
@@ -9,3 +9,30 @@
 // Например, если кинуть ему 3, то будет ждать 3 заресолвивишихся и потом сразу сам заресолвит, а если 3 не получится - реджектит.
 // Нужно сделать оптимально, чтоб когда уже 100% не хватит оставшихся промисов для ресолва, не ждать их, а сразу реджектить.
 
+
+const promiseMaker = function (msg, ms = 1000, switcher = true) {
+  if (switcher) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(msg);
+      }, ms);
+    });
+  } else {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject(new Error(msg));
+      }, ms);
+    });
+  }
+};
+
+
+Promise.myAny = async function ([...args]) {
+  return args;
+}
+
+const promise1 = promiseMaker('resolved1' ,1001);
+const promise2 = promiseMaker('resolved2' ,1000);
+const promise3 = promiseMaker('resolved3' ,1002);
+
+Promise.myAny([promise1, promise2, promise3]);
--- LearnJS/part1/chapter9_classes/9.1_class/p_9.1_class.js
+++ LearnJS/part1/chapter9_classes/9.1_class/p_9.1_class.js
@@ -1,3 +1,4 @@
+// Task 1
 // [ООП, глава 9] Задача с собеса
 // Сложность 6/10 на собесе, обычно 3/10
 // Дан код, ответить на вопросы к нему
@@ -8,7 +9,7 @@
 // 4. Куда записывается каждое свойство?
 // 5. Что выведется в результате выполнения for и spread? Почему?
 
-class Item {
+class Item1 {
   data = 10;
   get() {}
   static data = 20;
@@ -19,7 +20,7 @@ class Item {
 // for (const key in new Item()) { console.log(key); }
 
 // console.log({...Item});
-console.log({...new Item()});
+// console.log({...new Item1()});
 
 
 // 1) Разные типы свойств - статическое и публичное
@@ -34,33 +35,57 @@ console.log({...new Item()});
 
 
 
-
-
-// TODO выполнить задачу
+// Task 2
 // [ООП, главы 8,9] Задача с собеса
 // Сложность 8/10 на собесе, обычная 4/10
-// // With given example, need to write prototype analog
-// class BasicItem {
-//     constructor(_testProp) {
-//         this._parentProp = _testProp + 100;
-//     }
-//
-//     getParentProp() {
-//         return this._parentProp;
-//     }
-// }
-//
-// class Item extends BasicItem {
-//     static data = 5;
-//
-//     constructor(_testProp) {
-//         super(_testProp);
-//         this._testProp = _testProp;
-//     }
-//
-//     getProp() {
-//         return this._testProp + this.getParentProp() + Item.data;
-//     }
-// }
-//
-// log(new Item(1000).getProp()); // expect 2105
+// With given example, need to write prototype analog
+class CBasicItem {
+    constructor(_testProp) {
+        this._parentProp = _testProp + 100;
+    }
+
+    getParentProp() {
+        return this._parentProp;
+    }
+}
+
+class CItem extends CBasicItem {
+    static data = 5;
+
+    constructor(_testProp) {
+        super(_testProp);
+        this._testProp = _testProp;
+    }
+
+    getProp() {
+        return this._testProp + this.getParentProp() + CItem.data;
+    }
+}
+
+console.log(new CItem(1000).getProp()); // expect 2105
+
+
+
+// Решение: ------------------------------------------------------------------------------------------------------------
+function PBasicItem(_testProp) {
+  this._parentProp = _testProp + 100;
+
+  this.getParentProp = function () {
+    return this._parentProp;
+  }
+}
+
+function PItem(_testProp) {
+  Object.setPrototypeOf(PItem, PBasicItem);
+
+  PItem.data = 5;
+
+  this._testProp = _testProp;
+
+  this.getProp = function () {
+    const parent = new (Object.getPrototypeOf(Object.getPrototypeOf(this).constructor))(_testProp);     // хы
+    return this._testProp + parent.getParentProp() + PItem.data;
+  }
+}
+
+console.log(new PItem(1000).getProp());
